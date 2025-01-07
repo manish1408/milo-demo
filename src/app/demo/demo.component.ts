2dfe075e1e4537c30e2a9fd85357effd3219619b
Manish Gautam <manish.gautam.india@gmail.com>
2025-01-07 12:47:21 +0530

    Updates

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -11,19 +11,19 @@ import { ToastrService } from "ngx-toastr";
 import { ToastService } from "../_services/toast.service";
 import { finalize } from "rxjs";
 import { LeadService } from "../_services/leads.service";
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-demo',
-  templateUrl: './demo.component.html',
-  styleUrl: './demo.component.scss'
+  selector: "app-demo",
+  templateUrl: "./demo.component.html",
+  styleUrl: "./demo.component.scss",
 })
 export class DemoComponent {
-
   loading: boolean = false;
-  leadDetail:any;
-  id:any;
-  websiteURL:any;
+  leadDetail: any;
+  id: any;
+  websiteURL: any;
+  chatbotid: any;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -35,13 +35,32 @@ export class DemoComponent {
   ) {}
 
   ngOnInit() {
-    if(this.route.snapshot.queryParams["website"]) {
+    if (this.route.snapshot.queryParams["website"]) {
       this.id = this.route.snapshot.queryParams["website"];
-      this.websiteURL = this.sanitizer.bypassSecurityTrustResourceUrl("https://" + this.id); ;
+      this.websiteURL = this.sanitizer.bypassSecurityTrustResourceUrl(
+        "https://" + this.id
+      );
     }
     this.getLeadDetail(this.id);
   }
 
+  injectChatbotScript(chatbotId:any) {
+    // Create a new script element
+    const script = document.createElement("script");
+
+    // Set the src attribute with the chatbot ID as a query parameter
+    script.src = `https://app.miloassistant.ai/agent/build/chatbot.min.js?id=${encodeURIComponent(
+      chatbotId
+    )}`;
+
+    // Set the script type
+    script.type = "text/javascript";
+
+    // Append the script element to the <head> section
+    document.head.appendChild(script);
+
+    console.log(`Chatbot script injected with ID: ${chatbotId}`);
+  }
   openchatbot() {
     document.getElementById("chatbot-button-sightera")?.click();
   }
@@ -53,6 +72,8 @@ export class DemoComponent {
         next: (res: any) => {
           if (res?.success == true && res?.data) {
             this.leadDetail = res.data;
+            this.chatbotid = this.leadDetail.chatbot;
+            this.injectChatbotScript(this.chatbotid);
           } else {
             this.leadDetail = {};
           }
